441fa609b2b998b8b4219d9d6e098b4a0a035c0d
C0DERS0N <73357230+C0DERS0N@users.noreply.github.com>
2023-01-02 15:20:43 +0200

    Add files via upload

---
+++ Week14/Program.cs
@@ -0,0 +1,66 @@
+﻿Console.WriteLine("Your dog name:");
+string myDogName = Console.ReadLine();
+
+Dog myDog = new Dog(myDogName);
+Dog neighboursDog = new Dog("Good Girl!");
+
+Console.WriteLine($"My dog name is {myDog.Name}.");
+Console.WriteLine($"My neighbour dog name is  {neighboursDog.Name}.");
+
+
+myDog.Rename("Bad Boy");
+
+while (myDog.LevelOfHapiness != 5)
+{
+    myDog.Bark();
+}
+
+
+myDog.WagTail();
+
+class Dog   
+{
+    //fields
+    private string _name;
+    private int _levelOfHapiness;
+
+
+    //constructor
+    public Dog(string name)
+    {
+        _name = name;
+        _levelOfHapiness = 0;
+    }
+
+    //getter
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    public int PrecentOfHappiness
+    {
+        get { return _levelOfHapiness; }
+    }
+
+
+
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"The dog has been renamed to: {newName}.");
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine("Woof-woof!");
+        _levelOfHapiness++;
+    }
+
+    public void WagTail()
+    {
+        Console.WriteLine("Wigle-wigle!");
+    }
+
+}
